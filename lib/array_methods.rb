5c3a6448c894a657825913ac8a185ffddec764ba
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-13 19:17:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,8 +14,22 @@ end
 
 
 def find_max_value(array)
-  # Add your solution here
+
+length = array.length
+max = 0
+
+length.times do |index|
+  if array[index] > max
+    max = array[index]
+  end
 end
+
+return max
+end
+
+
+
+
 
 def find_min_value(array)
   # Add your solution here
