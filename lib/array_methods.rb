f79dd104c3275fc767f9881b0d43660cbdd6b2d2
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-13 19:15:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,10 @@
 def find_element_index(array, value_to_find)
   
 length = array.length
+
 length.times do |index|
   
-if array[index] = value_to_find
+if array[index] == value_to_find
     return index
  end
 end  
